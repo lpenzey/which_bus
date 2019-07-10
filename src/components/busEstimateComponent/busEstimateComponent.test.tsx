1da325f8c7b4ff3fb49050c540debbcadd07a1b7
Lucas PenzeyMoog <lucaspenzeymoog@gmail.com>
2019-07-10 16:05:34 -0500

    Refactor component test
    Formerly the test asserted against the state of the component to show
    that clicking the button updated the component with the values retrieved
    from the busTrackerAPI. Now it uses snapshots to show that the change is
    actually displayed as expected.

--- src/components/busEstimateComponent/busEstimateComponent.test.tsx
+++ src/components/busEstimateComponent/busEstimateComponent.test.tsx
@@ -4,24 +4,33 @@ import { mount } from 'enzyme';
 import Enzyme from 'enzyme';
 import StubBusTrackerAPI from '../../Services/StubBusTrackerAPI'
 import Adapter from 'enzyme-adapter-react-16';
+import toJSON from 'enzyme-to-json';
 
 function flushPromises() {
     return new Promise(resolve => setImmediate(resolve));
 }
 
 describe('<BusEstimateComponent />', () => {
-    it('updates state with bus tracker api values', () => {
-        Enzyme.configure({ adapter: new Adapter() })
-        const wrapper = mount(<BusEstimateComponent api={StubBusTrackerAPI} />);
+    Enzyme.configure({ adapter: new Adapter() })
+    it('does a snapshot', () => {
+        const wrapper = mount(
+            <BusEstimateComponent api={StubBusTrackerAPI} />
+        );
+
+        expect(toJSON(wrapper)).toMatchSnapshot();
+    });
+
+    it('should update text when clicking on button', () => {
+        const wrapper = mount(
+            <BusEstimateComponent api={StubBusTrackerAPI} />
+        );
 
         const button = wrapper.find('#estimateButton').at(0);
         button.simulate('click');
 
         return flushPromises().then(() => {
-            expect(wrapper.state('route')).toEqual("20");
-            expect(wrapper.state('time')).toEqual("12:02");
-            expect(wrapper.state('stopName')).toEqual("Madison & Jefferson");
-            expect(wrapper.state('direction')).toEqual("Westbound");
+            wrapper.update()
+            expect(toJSON(wrapper)).toMatchSnapshot();
         });
     });
 });
