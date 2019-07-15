2f70b96df232b8cdfc990dc9dfb98d1bc46aaf24
Lucas PenzeyMoog <lucaspenzeymoog@gmail.com>
2019-07-15 16:19:31 -0500

    Add config to toggle mock vs. real api
    This config boolean propagates the desired api (real or mocked)
    throughout the program. If the mocked api is selected, the components
    will pull their data from the json files in the __fixtures__ folder.

---
+++ src/configs/App.ts
@@ -0,0 +1,3 @@
+export const appConfig = {
+    useMockApi: false
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import BusEstimate from './components/busEstimate/busEstimate';
 import NavBar from './components/navBar/navBar';
 import BusTrackerAPI from './Services/busTrackerAPI';
-
+import { appConfig } from 'configs/App';
+import StubBusTrackerAPI from 'Services/stubBusTrackerAPI';
 
 const Main: React.FC = () => {
+    const busTrackerApi = appConfig.useMockApi ? StubBusTrackerAPI : BusTrackerAPI;
 
     return (
         <div>
             <NavBar />
             <br></br>
-            <BusEstimate api={BusTrackerAPI} />
+            <BusEstimate api={busTrackerApi} />
         </div>
     );
 }
