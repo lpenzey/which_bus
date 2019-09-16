cc72222488568d3021b63a730a2d600e8cb27c91
Lucas PenzeyMoog <lucaspenzeymoog@gmail.com>
2019-09-16 09:34:45 -0500

    Generify button component

---
+++ src/components/genericButton/genericButtonTest.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import UpdateButton from "./genericButton";
+import {
+  render,
+  fireEvent,
+  cleanup,
+  waitForElement
+} from "@testing-library/react";
+
+afterEach(cleanup);
+it("Updates the selected bus stops", async () => {
+  //   expect(1).toMatchSnapshot();
+});
