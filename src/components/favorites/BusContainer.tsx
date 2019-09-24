55097ab8aa52e60ed4eba146090f2d87b67aaead
Lucas PenzeyMoog <lucaspenzeymoog@gmail.com>
2019-09-24 13:13:05 -0500

    Render favorites with individual estimates

--- src/components/favorites/BusContainer.tsx
+++ src/components/favorites/BusContainer.tsx
@@ -1,55 +1,32 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useEffect, useState } from "react";
 import BusArrival from "components/busArrival/busArrival";
+import { Wrapper } from "styles/theme";
 
 export default function BusContainer(props: any) {
+  const [estimates, setEstimates] = useState(Array<any>());
+  const [update, setUpdate] = useState(0);
+
   function handleChange(e: any) {
     props.handleChange(e.target.value);
   }
 
-  const Wrapper = styled.div`
-    border-radius: 4px;
-    background-color: peachpuff
-    box-shadow: 0 4px 6px 0 hsla(0, 0%, 0%, 0.2);
-    display: flex;
-    flex-wrap: wrap;
-    padding: 2px;
-    justify-content: center;
-    width: 100%;
-    margin: 1em auto;
-    min-width: 250px;
-    max-width: 400px;
-
-    @media (min-width: 400px) {
-      padding: 1.5em;
-      width: 60%;
-      display: flex;
-    }
-
-    @media (min-width: 1000px) {
-      width: 60%;
-      display: inline-flex;
-      justify-content: center;
-      flex-direction: row;
-      margin: 1em;
-    }
-  `;
-
-  const WrapperInner = styled.div`
-    padding: 0.5em;
-    width: 100%;
-    margin: 0.5em;
-    justify-content: center;
-  `;
-
   return (
     <Wrapper>
       <h1>Route {props.route}</h1>
-      {props.estimates.map(
-        (item: { time: string; delay: boolean; display: React.ReactNode }) => (
-          <BusArrival time={item.time} display="block" delay={item.delay} />
-        )
-      )}
+      {props.estimates &&
+        props.estimates.map(
+          (item: {
+            time: string;
+            delay: boolean;
+            display: React.ReactNode;
+          }) => (
+            <BusArrival
+              time={item.time}
+              display="block"
+              key={props.estimates.time}
+            />
+          )
+        )}
     </Wrapper>
   );
 }
